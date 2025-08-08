c5d76963e1baa70ba7048989df1d64c5352fa723
Mohammed-mm1 <mohammed707ksa1@gmail.com>
2025-08-09 01:23:01 +0300

    change from hi to welcome

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-System.out.println("hi000066");
+System.out.println("hgi000066");
 System.out.println("welcome ");
 System.out.println("welcome ");
 
